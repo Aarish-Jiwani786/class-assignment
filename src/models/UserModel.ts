8574fb8e97e9d1bf972e99d1aeeb8df415654523
Aarish Jiwani <aarish.jiwani@smail.astate.edu>
2023-03-01 09:07:59 -0600

    Changes

---
+++ src/models/UserModel.ts
@@ -0,0 +1,22 @@
+import { AppDataSource } from '../dataSource';
+import { User } from '../entities/User';
+
+const userRepository = AppDataSource.getRepository(User);
+
+async function addUser(email: string, passwordHash: string): Promise<User> {
+  // 1) Create a new user object and set the properties
+  let newUser = new User();
+  newUser.email = email;
+  newUser.passwordHash = passwordHash;
+  // 2) Save it in the database
+  newUser = await userRepository.save(newUser);
+  // 3) Return the created user
+  return newUser;
+}
+
+async function getUserByEmail(email: string): Promise<User | null> {
+  const user = await userRepository.findOne({ where: { email } });
+  return user;
+}
+
+export { addUser, getUserByEmail };
